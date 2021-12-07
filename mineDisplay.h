308458ba2fdfe834547e8c2451855b514a36ef6a
Topher <topher.johnson10@gmail.com>
2021-12-06 23:49:13 -0700

    Made a touchHandler to tell if it's a long press or not

--- mineDisplay.h
+++ mineDisplay.h
@@ -17,7 +17,17 @@ void mineDisplay_drawMine(uint8_t x, uint8_t y, bool redBackground);
 // draws the tile with its threat level or nothing.
 void mineDisplay_revealTile(uint8_t x, uint8_t y, uint8_t theatLvl);
 
+//draws the text at the beginning of the game. erases it if erase == true
+void mineDisplay_drawGameIntro(bool erase);
 
+//draws the flag on an untouched tile, redraws the tile color if erawe == true
+void mineDisplay_drawFlag(bool erase);
+
+//draws a flagged mine after the game is over, maybe a green background behind the flag
+void mineDisplay_drawFlaggedMine();
+
+//draws a flagged piece of dirt once the game is over (red x in some versions)
+void mineDisplay_drawFlaggedDirt();
 
 
 
